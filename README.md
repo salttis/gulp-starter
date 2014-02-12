4c1fea126acd5b0babb0a28563add52081cd0031
Juri Saltbacka <juri.saltbacka@gmail.com>
2014-02-12 12:07:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Gulp Starter
+
+A GulpJS starter template with basic tasks for development and production.
+
+* Source: [http://github.com/3bola/gulp-starter](http://github.com/3bola/gulp-starter)
+* Issues: [https://github.com/3bola/gulp-starter/issues](https://github.com/3bola/gulp-starter/issues)
+
+### Features
+
+* Simple HTML5 boilerplate
+* Separated production and development environments
+* Using SCSS
+* Livereloading development server with automatic building of SCSS files
+* Bower component management
+* Automatic image compressing
+
+### Usage
+
+#### Install
+
+Make sure you have [NodeJS](http://nodejs.org/) installed.
+
+Run:
+```sh
+git clone https://github.com/3bola/gulp-starter.git && cd ukko && npm install
+```
+
+#### Running the development server
+
+Run:
+```sh
+gulp
+```
+
+#### Building the application
+
+Run:
+```sh
+gulp build
+```
+
+### License
+
+#### The MIT License (MIT)
+
+Copyright (c) Juri Saltbacka
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+[![githalytics.com alpha](https://cruel-carlota.pagodabox.com/d3d546fd33d30403d8ccfb5934f8d4f0 "githalytics.com")](http://githalytics.com/3bola/gulp-starter)
---
+++ gulpfile.js
@@ -0,0 +1,114 @@
+"use strict";
+
+var gulp         = require('gulp')
+  , gutil        = require('gulp-util')
+  , sass         = require('gulp-sass')
+  , refresh      = require('gulp-livereload')
+  , prefix       = require('gulp-autoprefixer')
+  , minify       = require('gulp-minify-css')
+  , imagemin     = require('gulp-imagemin')
+  , uglify       = require('gulp-uglify')
+  , clean        = require('gulp-clean')
+  , tinylr       = require('tiny-lr')
+  , express      = require('express')
+  , path         = require('path')
+  , info         = require('./package.json')
+  , lr;
+
+// Configuration
+
+var Config = {
+  port: 8080,
+  livereload_port: 35729,
+  open_browser: true,
+  images: {
+    compression: 3,
+    progressive: true,
+    interlaced: true
+  }
+}
+
+// Paths
+
+var paths = {
+  app:   {
+    root:   './app',
+    js:     './app/js',
+    scss:   './app/scss',
+    css:    './app/css',
+    images: './app/img',
+    lib:    './app/lib'
+  },
+  build: {
+    root:   './build',
+    js:     './build/js',
+    css:    './build/css',
+    images: './build/img',
+    lib:    './build/lib'
+  }
+}
+
+// Tasks
+
+gulp.task('styles', function(){
+  gulp.src(paths.app.scss + '/**/*.scss')
+    .pipe(sass())
+    .pipe(prefix('last 2 version', '> 5%', 'safari 5', 'ie 8', 'ie 7', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest(paths.app.css));
+});
+
+gulp.task('server', function(){
+  var server = express()
+    .use(express.static(path.resolve(paths.app.root)))
+    .listen(Config.port);
+  gutil.log('Server listening on port', Config.port);
+});
+
+gulp.task('livereload', function(){
+  lr = tinylr();
+  lr.listen(Config.livereload_port, function(err) {
+    if(err) gutil.log('Livereload error:', err);
+  })
+});
+
+gulp.task('watch', function(){
+  gulp.watch(paths.app.scss + '/**/*.scss', ['styles']);
+  gulp.watch([paths.app.css + '/**/*', paths.app.root + '/**/*.html'], function(evt){
+    refresh(lr).changed(evt.path);
+  })
+});
+
+gulp.task('build', function(){
+
+  // Clean
+  gulp.src(paths.build.root + '/**/*', { read: false })
+    .pipe(clean());
+
+  // Scripts
+  gulp.src(paths.app.js + '/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.build.js));
+
+  // Styles
+  gulp.src(paths.app.scss + '/**/*.scss')
+    .pipe(sass())
+    .pipe(prefix('last 2 version', '> 5%', 'safari 5', 'ie 8', 'ie 7', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(minify())
+    .pipe(gulp.dest(paths.build.css));
+
+  // Images
+  gulp.src(paths.app.images + '/**/*')
+    .pipe(imagemin({ optimizationLevel: Config.images.compression, progressive: Config.images.progressive, interlaced: Config.images.interlaced }))
+    .pipe(gulp.dest(paths.build.images));
+
+  // Libraries
+  gulp.src(paths.app.lib + '/**/*')
+    .pipe(gulp.dest(paths.build.lib));
+
+  // HTML files
+  gulp.src(paths.app.root + '/**/*.html')
+    .pipe(gulp.dest(paths.build.root));
+
+});
+
+gulp.task('default', ['server', 'livereload', 'styles', 'watch']);
